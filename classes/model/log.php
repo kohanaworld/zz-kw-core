8414a05bd88d1a0596450052eb46f66526d9ac3f
biakaveron <biakaveron@gmail.com>
2011-02-16 00:12:47 +0300

    added Log model

---
+++ classes/model/log.php
@@ -0,0 +1,26 @@
+<?php defined('SYSPATH') OR die('No direct access allowed.');
+
+class Model_Log extends Jelly_Model {
+
+	public static function initialize(Jelly_Meta $meta)
+	{
+		$meta->fields(array(
+			'id'           => Jelly::field('primary'),
+			'executant'    => Jelly::field('string', array(
+				'allow_null'   => FALSE,
+			)),
+			'executant_id' => Jelly::field('integer'),
+			'action'       => Jelly::field('string', array(
+				'allow_null'   => FALSE,
+			)),
+			'text'         => Jelly::field('string'),
+			'date'         => Jelly::field('timestamp', array(
+				'auto_now_create' => TRUE,
+			)),
+			// @TODO enum field? Log levels must be defined as constants
+			'level'        => Jelly::field('string'),
+		));
+	}
+
+
+}
